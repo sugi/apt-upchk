3997c1a933c4d2da32a6be3350f5f97db9784c25
sugi <sugi@b668cab4-68fd-0310-8e7c-84d56a6c3f58>
2005-10-12 08:57:42 +0000

    add: monthly full report
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/debian/apt-upchk/trunk@154 b668cab4-68fd-0310-8e7c-84d56a6c3f58

--- debian/cron.d
+++ debian/cron.d
@@ -3,4 +3,6 @@
 #
 PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 
-0 */6	* * *	aptupchk	test -x /usr/share/apt-upchk/scripts/notify && /usr/share/apt-upchk/scripts/notify
+7 */6	2-31 * *	aptupchk	test -x /usr/share/apt-upchk/scripts/notify && /usr/share/apt-upchk/scripts/notify
+7 1-23/6	1 * *	aptupchk	test -x /usr/share/apt-upchk/scripts/notify && /usr/share/apt-upchk/scripts/notify
+7 0	1 * *	aptupchk	test -x /usr/share/apt-upchk/scripts/notify && /usr/share/apt-upchk/scripts/notify -f
